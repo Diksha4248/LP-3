bc8289dd51e7919500436130b6062157582caa47
Diksha4248 <88928334+Diksha4248@users.noreply.github.com>
2023-10-18 12:50:49 +0530

    Ml Changes

--- DAA/N-queen.cpp
+++ DAA/N-queen.cpp
@@ -6,7 +6,6 @@
 #define N 4
 using namespace std;
 
-// A utility function to print solution
 void printSolution(int board[N][N])
 {
 	for (int i = 0; i < N; i++) {
@@ -18,12 +17,6 @@ void printSolution(int board[N][N])
 	}
 }
 
-// A utility function to check if a queen can
-// be placed on board[row][col]. Note that this
-// function is called when "col" queens are
-// already placed in columns from 0 to col -1.
-// So we need to check only left side for
-// attacking queens
 bool isSafe(int board[N][N], int row, int col)
 {
 	int i, j;
@@ -46,8 +39,6 @@ bool isSafe(int board[N][N], int row, int col)
 	return true;
 }
 
-// A recursive utility function to solve N
-// Queen problem
 bool solveNQUtil(int board[N][N], int col)
 {
 	// base case: If all queens are placed
@@ -59,37 +50,19 @@ bool solveNQUtil(int board[N][N], int col)
 	// this queen in all rows one by one
 	for (int i = 0; i < N; i++) {
 		
-		// Check if the queen can be placed on
-		// board[i][col]
 		if (isSafe(board, i, col)) {
 			
-			// Place this queen in board[i][col]
 			board[i][col] = 1;
 
-			// recur to place rest of the queens
 			if (solveNQUtil(board, col + 1))
 				return true;
-
-			// If placing queen in board[i][col]
-			// doesn't lead to a solution, then
-			// remove queen from board[i][col]
 			board[i][col] = 0; // BACKTRACK
 		}
 	}
-
-	// If the queen cannot be placed in any row in
-	// this column col then return false
 	return false;
 }
 
-// This function solves the N Queen problem using
-// Backtracking. It mainly uses solveNQUtil() to
-// solve the problem. It returns false if queens
-// cannot be placed, otherwise, return true and
-// prints placement of queens in the form of 1s.
-// Please note that there may be more than one
-// solutions, this function prints one of the
-// feasible solutions.
+
 bool solveNQ()
 {
 	int board[N][N] = { { 0, 0, 0, 0 },
